b6f2e106a84c2be1958d9b35c899fb31c0680f1b
Oskar <oskarradonkimball@gmail.com>
2016-08-22 13:08:25 -0700

    Import html

--- pokemon-react/src/App.js
+++ pokemon-react/src/App.js
@@ -6,13 +6,59 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 class="title">Pokedex</h1>
+
+        <h2 class="Orbitron">Welcome to Pokedex!</h2>
+
+        <section class="search">
+          <p>Search for a pokemon by name.</p>
+          <form class="poke-search" method="post">
+            <input type="text" name="name" value="" class="searchbar" />
+            <input name="name" value="Search" type="submit" />
+          </form>
+          <i class="fa fa-spinner fa-pulse fa-3x fa-fw loading-spinner"></i>
+        </section>
+
+        <section class="search-results">
+          <p class="error-message"></p>
+          <img class="pokeSprite" src="" alt="" />
+          <div class="stats">
+            <div class="hp"><p>HP</p></div>
+            <div class="attack"><p>Attack</p></div>
+            <div class="defense"><p>Def</p></div>
+            <div class="special-attack"><p>Special Attack</p></div>
+            <div class="special-defense"><p>Special Defense</p></div>
+            <div class="speed"><p>Speed</p></div>
+          </div>
+          <button type="button" class="add">Add to team</button>
+        </section>
+
+        <section class="team">
+          <div id="pokemon0">
+            <img src="" alt="" />
+          </div>
+
+          <div id="pokemon1">
+            <img src="" alt="" />
+          </div>
+
+          <div id="pokemon2">
+            <img src="" alt="" />
+          </div>
+
+          <div id="pokemon3">
+            <img src="" alt="" />
+          </div>
+
+          <div id="pokemon4">
+            <img src="" alt="" />
+          </div>
+
+          <div id="pokemon5">
+            <img src="" alt="" />
+          </div>
+        </section>
+        <button type="button" class="remove">Remove selected</button>
       </div>
     );
   }
